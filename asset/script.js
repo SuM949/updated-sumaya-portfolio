17e978feefd923c4ad7582b5a30df234b627d058
Sumaya Gure <sumayabile8@gmail.com>
2022-02-19 16:26:41 +0000

    updating

--- asset/script.js
+++ asset/script.js
@@ -1,19 +1,26 @@
-const links = document.querySelectorAll('.link');
-const sections = document.querySelectorAll('section');
-
+const links = document.querySelector('.link');
+const sections = document.querySelector('section');
+const navBtn = document.querySelector('.navBtn')
 let activeLink = 0;
 
-links.forEach((link, i) => {
-    link.addEventListener('click', () => {
-         if(activeLink != i){
-            links[activeLink].classList.remove('active');
-            link.classList.add('active');
-            sections[activeLink].classList.remove('active');
+// links.forEach((link, i) => {
+//     link.addEventListener('click', () => {
+//          if(activeLink != i){
+//             link[activeLink].classList.remove('active');
+//             link.classList.add('active');
+//             sections[activeLink].classList.remove('active');
 
-            setTimeout(() => {
-                activeLink = i;
-                sections[i].classList.add('active');
-            },1000);
-        }
-    })
+//             setTimeout(() => {
+//                 activeLink = i;
+//                 sections[i].classList.add('active');
+//             },1000);
+//         }
+//     })
+// })
+
+
+navBtn.addEventListener('click', () => {
+    
+     const linkClick = this.event.target
+     console.log(linkClick)
 })
